72ed5796b810d87106cb549fc70b60d6bf66f648
funeralparlor <lancedeviant@gmail.com>
2025-03-05 15:29:03 +0800

    init deploy

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173'],
+    'allowed_origins' => ['https://vue-frontend-8ioa.onrender.com'],
 
     'allowed_origins_patterns' => [],
 
